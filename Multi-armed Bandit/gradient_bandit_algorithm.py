b5500f9f556d9f1571f667f1414334d1b5f2f8f9
Alonso Granados <alonsog@email.arizona.edu>
2020-09-01 19:03:11 -0600

    Gradient algorithm

--- Multi-armed Bandit/gradient_bandit_algorithm.py
+++ Multi-armed Bandit/gradient_bandit_algorithm.py
@@ -1,13 +1,6 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
-# Q_values: Estimated value of an action
-# N_values: Number of actions chosen
-# R_values: Rewards over time
-# a: Selected action
-# armedBandits: Distributions
-# e: Threshold to select greedy action
-def e_greedy_algorithm(steps,samples,number_bandits, e):
+def gradient_bandit_algorithm(steps,samples,number_bandits, alpha):
     total_R_values = np.zeros(steps)
     average_optimal_action = 0
 
@@ -15,24 +8,23 @@ def e_greedy_algorithm(steps,samples,number_bandits, e):
         #True mean distributions
         armedBandits = np.random.randn(number_bandits)
         Best_action = np.argmax(armedBandits)
-        Q_values = np.zeros(number_bandits)
-        N_values = np.zeros(number_bandits)
+        average_reward = 0
+        H_values = np.zeros(number_bandits)
         R_values = np.zeros(steps)
+        prob = np.ones(number_bandits)/number_bandits
         optimal_action = np.zeros(steps)
         for i in range(steps):
-            random_action = np.random.rand(1)
-            if(random_action > e):
-                a = np.argwhere(Q_values==np.max(Q_values)).flatten()
-                index = np.random.randint(a.shape[0])
-                a = a[index]
-            else:
-                a = np.random.randint(10)
+            a = np.random.choice(number_bandits, 1, p=prob)
+            # print(a)
             if(a == Best_action):
                 optimal_action[i] = 1
             R = np.random.randn(1) + armedBandits[a]
+            average_reward += (R - average_reward)/(i+1)
+            temp_H = H_values[a]
+            H_values += - alpha*(R - average_reward)* prob
+            H_values[a] = temp_H + alpha*(R - average_reward)*(1-prob[a])
             R_values[i] = R
-            N_values[a] += 1
-            Q_values[a] += (R - Q_values[a])/N_values[a]
+            prob = np.exp(H_values)/np.sum(np.exp(H_values))
         # Average reward over time
         N = np.arange(steps)+1
         R_values = np.cumsum(R_values)/N
--- Multi-armed Bandit/test.py
+++ Multi-armed Bandit/test.py
@@ -1,6 +1,8 @@
 from e_greedy_algorithm import e_greedy_algorithm
 from UPC import UPC
+from gradient_bandit_algorithm import gradient_bandit_algorithm
 import matplotlib.pyplot as plt
+
 steps = 2000
 samples = 1000
 number_bandits = 10
@@ -8,25 +10,36 @@ number_bandits = 10
 rewards = []
 optimal_actions = []
 
-# E_greedy_algorithm
+algorithms = ["E_greedy", "UPC", "Gradient"]
+
+# E Greedy Algorithm
 average_reward, average_optimal_action = e_greedy_algorithm(steps, samples, number_bandits, 0.1)
 rewards.append(average_reward)
 optimal_actions.append(average_optimal_action)
+
 # UPC
 average_reward, average_optimal_action = UPC(steps, samples, number_bandits, 2)
 rewards.append(average_reward)
 optimal_actions.append(average_optimal_action)
 
-# Plot Rewards
-plt.title("Average reward over time")
-plt.plot(rewards[0], label="E_greedy")
-plt.plot(rewards[1], label="UPC")
-plt.legend()
-plt.show()
+# Gradient Bandit Algorithm
+average_reward, average_optimal_action = gradient_bandit_algorithm(steps, samples, number_bandits, 0.1)
+rewards.append(average_reward)
+optimal_actions.append(average_optimal_action)
 
+# Plot Rewards
+plt.figure(1)
+plt.title("Average reward over time")
 # Plot Optimal Actions
+plt.figure(2)
 plt.title("Average optimal action")
-plt.plot(optimal_actions[0], label="E_greedy")
-plt.plot(optimal_actions[1], label="UPC")
+for i in range(len(algorithms)):
+    plt.figure(1)
+    plt.plot(rewards[i], label=algorithms[i])
+    plt.figure(2)
+    plt.plot(optimal_actions[i], label=algorithms[i])
+
+plt.legend()
+plt.figure(1)
 plt.legend()
 plt.show()
